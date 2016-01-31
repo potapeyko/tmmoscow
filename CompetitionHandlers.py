53120c1d0477e0c152695b00462b9e1e92f09097
DariaPlotnikova <plotnikovanstu@gmail.com>
2016-01-31 21:51:36 +0600

    Changed interface on page "Add new competition"

--- CompetitionHandlers.py
+++ CompetitionHandlers.py
@@ -22,7 +22,13 @@ class NewCompetition(webapp2.RequestHandler):
             temp_values = {'img_src':'../static/img/er401.png', 'er_name':'401', 'login_redir':users.create_login_url('/reg/newCompetition')}
             self.response.write(JINJA_ENVIRONMENT.get_template('templates/tmmosc/ErrorPage.html').render(temp_values))
     def post(self): # save filled form about new competition
-        self.response.write('POST Save new info about competition (ORG)')
+        user = users.get_current_user()
+        if user:
+            email = user.email()
+
+            temp_values = {'user_email':email, 'logout':users.create_logout_url('/login')}
+            template = JINJA_ENVIRONMENT.get_template('templates/tmmosc/organizer/AddCompetition.html')
+            self.response.write(template.render(temp_values))
 
 class CertainCompetition(webapp2.RequestHandler):
     def get(self):
